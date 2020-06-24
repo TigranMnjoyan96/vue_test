c04684040f3b4ca88c2023b09f37c67c9735d6a1
TigranMnjoyan96 <48060335+TigranMnjoyan96@users.noreply.github.com>
2020-06-25 00:00:53 +0400

    first

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav>
+        <div class="nav-wrapper">
+            <router-link to="/" class="brand-logo">Logo</router-link>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <router-link tag="li"
+                             to="/"
+                             exact active-class="active"
+                ><a>Sass</a></router-link>
+                <router-link tag="li"
+                             to="/smth"
+                              active-class="active"
+                ><a>sdvk</a></router-link>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: "Navbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
